cd04878d871e0d320e67ba83b94c561a981edf97
mpowalisz <markpowalisz@gmail.com>
2018-10-21 15:56:54 -0400

    Added src array

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,15 @@ document.getElementById("card-1").addEventListener("click", flip);
 function flip() {
   console.log("card flips");
 }
+
+let img_src = [
+  ".assets/images/scarlett-witch.jpg",
+  ".assets/images/captain_america.jpg",
+  ".assets/images/black_panther.jpg",
+  ".assets/images/black-widow.jpg",
+  ".assets/images/nick-fury.jpg",
+  ".assets/images/spider-man.png",
+  ".assets/images/thor.jpg",
+  ".assets/images/iron-man.jpg",
+  ".assets/images/hulk.png"
+];
